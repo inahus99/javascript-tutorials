c6185620eec4141c757d3dd3f16c43eed72a4bbb
inahus99 <165036067+inahus99@users.noreply.github.com>
2024-06-14 22:56:12 +0530

    Add files via upload

---
+++ comparisons.js
@@ -0,0 +1,20 @@
+//basics
+// console.log(2>1)
+// console.log(4<=5)
+//different dataypes
+// console.log("456"<90)
+
+//null comaprisons
+//comaparisons and equality works differntly in javascript, in comaprison null is converted into number 0
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+// same for undefined but false in all three
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>=0)
+
+
+//===
+console.log("2"===2)
