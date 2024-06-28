2d77a115a8147dd862564ab2f52032e115f0eb3d
inahus99 <165036067+inahus99@users.noreply.github.com>
2024-06-28 23:33:27 +0530

    Add files via upload

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,17 @@
+console.log(3+3)
+//types of datatype in javascript
+// Null	"object"	standalone value
+// Undefined	"undefined"	
+// Boolean	"boolean" true/false	
+// Number	"number" general number	
+// BigInt	"bigint"	 very latge numbers
+// String	"string"	
+// Symbol	"symbol"  to mark uniqueness
+
+//object
+let value=undefined
+ let state=null;
+let age=15
+// console.log("suhani")
+console.log(typeof(null))
+console.log(typeof(undefined))
