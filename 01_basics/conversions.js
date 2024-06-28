cabd791fcb00c014b74e0bd183fcc4f7a8c8f84d
inahus99 <165036067+inahus99@users.noreply.github.com>
2024-06-28 23:32:56 +0530

    Add files via upload

---
+++ 01_basics/conversions.js
@@ -0,0 +1,13 @@
+// let score="suhani"
+// let ValueInNumber=Number(score)
+// console.log(typeof(ValueInNumber))
+// console.log(ValueInNumber)
+
+// let isLoggedIn=0;
+// let boolean=Boolean(isLoggedIn);
+// console.log(boolean);
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
